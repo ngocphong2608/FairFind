bb3c1cb43b15e6b182ebf3c1c9c0fb555c4c1caf
Phong Tran Thanh <ttphong@tma.com.vn>
2016-08-31 18:25:07 +0700

    Added splash activity

---
+++ app/src/main/java/com/tma/ttphong/fairfind/activity/MainActivity.java
@@ -0,0 +1,21 @@
+package com.tma.ttphong.fairfind.activity;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+
+import com.tma.ttphong.fairfind.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.splashScreenTheme);
+        super.onCreate(savedInstanceState);
+
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
